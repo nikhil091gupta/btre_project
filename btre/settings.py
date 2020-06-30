28c48b78cb73129406ceffe062117938c0d780d7
unknown <nikhil091gupta@gmail.com>
2020-07-01 00:02:17 +0530

    Adding back databases section

--- btre/settings.py
+++ btre/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -89,7 +89,7 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
